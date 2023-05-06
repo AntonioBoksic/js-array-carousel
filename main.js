52054d0b79a1eb589aff1ef29603e17d0ff0ebc9
Antonio <antonioboksic1603@gmail.com>
2023-05-06 03:22:50 +0200

    finito le milestone, devo solo capire perchè non funziona btnPrevious sul click, dice itemNodeList undefined, ma non capisco perchè l'ho dichiarata con scope globale e l'ho usata nell'altro bottone senza problemi

--- main.js
+++ main.js
@@ -1,24 +1,80 @@
-// seleziono gli item per poterli gestire
-const items= document.querySelectorAll(".item")
-console.log(items)
+    // MACROSTRUTTURA
 
-// questo mi dà comeoutput una NODE LIST con 5 elementi dentro
-// questo sarebbe un oggetto simile ad un array
-// qual è la differenza?
+    // INSERISCO IMMAGINI IN HTML
+        // INPUT
+        const myArr = [
+            "01.webp",
+            "02.webp",
+            "03.webp",
+            "04.webp",
+            "05.webp"
+        ];
+        console.log(myArr);
 
-// setto un indice per segnare l'item attivo al momento
-let activeItem = 0
+        // creazione variabile container in cui andranno le immagini
+        const itemsContainer = document.getElementById("itemsContainer");
 
-// seleziono next
-const next = document.querySelector(".next")
-console.log(next)
 
-// gestisco l'evento sul click su next
 
-next.addEventListener("click",
-function() {
-console.log("hai cliccato");
-// togli classe active all'elemento attivo
-items[activeItem]
-}
-);
+        for (let i = 0; i<myArr.length; i++) {
+            // const item = myArr[i];
+            // console.log(item)
+
+            const divElement = `<div class="item">
+            <img src="img/0${i+1}.webp" alt="">
+            </div>`;
+            // const divElement = document.createElement("div");
+            // divElement.innerHTML = `<img src="img/${item}">`
+            // console.log(divElement);
+
+            itemsContainer.innerHTML += divElement;
+            // itemsContainer.append(divElement)
+        };
+
+        // SELEZIONIAMO IMMAGINE 0 PER DARE CLASSE ACTIVE   
+        let imagePosition = 0;
+        let itemNodeList = document.querySelectorAll("#itemsContainer .item");
+        console.log(itemNodeList);
+        // questa è l'immagine attiva, che cambia dopo i click
+        itemNodeList[imagePosition].classList.add("active");
+
+ 
+
+        // QUANDO CLICCO AVANTI
+        const btnNext = document.getElementById("btnNext");
+        btnNext.addEventListener("click",
+            function() 
+            {
+                itemNodeList[imagePosition].classList.remove("active");
+
+                if (imagePosition == myArr.length-1) {
+                imagePosition=-1
+                }
+
+                imagePosition++;
+
+                itemNodeList[imagePosition].classList.add("active");
+                console.log(imagePosition);
+            }
+        );
+
+        // QUANDO CLICCO INDIETRO
+        const btnPrevious = document.getElementById("btnPrevious");
+        btnPrevious.addEventListener("click",
+            function() 
+            {
+
+                itemNodeList[imagePosition].classList.remove("active");
+
+                if (imagePosition == 0) {
+                imagePosition == myArr.length;
+                }
+
+                imagePosition= imagePosition-1;
+
+                itemNodeList[imagePosition].classList.add("active");
+                console.log(imagePosition);
+            }
+        );
+
+     
